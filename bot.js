582ddb070fe54eb763ef26e6b7b323175b9e1d18
Star1337 <36797886+Star1337@users.noreply.github.com>
2018-05-27 17:03:41 +0300

    message reaction add

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function color () {
 bott.on('ready', () => {
   console.log(`Logged in as ${bott.user.tag}!`);
   bott.user.setPresence({ game: { name: `на сервер`, type: 3 } }).catch();
-  bott.channels.get ('450295002132774924').fetchMessage ('450295197738467328');
+  bott.channels.get ('450295002132774924').fetchMessage ('450297775037349908');
 color ();
 });
 
